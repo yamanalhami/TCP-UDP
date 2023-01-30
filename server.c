d831b26b5c85eaaff89bf60e879d10b3ad49a076
yamanalhami <123995934+yamanalhami@users.noreply.github.com>
2023-01-31 00:05:20 +0300

    Add files via upload

---
+++ server.c
@@ -0,0 +1,111 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <ctype.h>
+#define MAXLINE 1024
+int max(int x, int y)
+{
+    if (x > y)
+        return x;
+    else
+        return y;
+}
+
+int main(int argc, char** argv)
+{
+    if(argc != 2)
+	{
+		printf("You should enter PORT number JUST.\n");
+		exit(-1);
+	}
+	
+	 if(atoi(argv[1]) < 1024 || atoi(argv[1]) > 65535)
+     {printf("enter true port number \n"); exit(-1);}
+     
+     
+    int listenfd, connfd, udpfd, nready, maxfdp1;
+    char buffer[MAXLINE];
+    pid_t childpid;
+    fd_set rset;
+    ssize_t n;
+    socklen_t len;
+    const int on = 1;
+    struct sockaddr_in cliaddr, servaddr;
+    void sig_chld(int);
+
+    /* create listening TCP socket */
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    //servaddr.sin_port = htons(PORT);
+    servaddr.sin_port = htons(atoi(argv[1]));
+    // binding server addr structure to listenfd
+    bind(listenfd, (struct sockaddr*)&servaddr, sizeof(servaddr));
+    listen(listenfd, 10);
+
+    /* create UDP socket */
+    udpfd = socket(AF_INET, SOCK_DGRAM, 0);
+    // binding server addr structure to udp sockfd
+    bind(udpfd, (struct sockaddr*)&servaddr, sizeof(servaddr));
+
+    // clear the descriptor set
+    FD_ZERO(&rset);
+
+    // get maxfd
+    maxfdp1 = max(listenfd, udpfd) + 1;
+    for (;;) {
+
+        // set listenfd and udpfd in readset
+        FD_SET(listenfd, &rset);
+        FD_SET(udpfd, &rset);
+
+        // select the ready descriptor
+        nready = select(maxfdp1, &rset, NULL, NULL, NULL);
+
+        // if tcp socket is readable then handle
+        // it by accepting the connection
+        if (FD_ISSET(listenfd, &rset)) {
+            len = sizeof(cliaddr);
+            connfd = accept(listenfd, (struct sockaddr*)&cliaddr, &len);
+            if ((childpid = fork()) == 0) {
+                close(listenfd);
+                bzero(buffer, sizeof(buffer));
+                printf("Message From TCP client: ");
+                read(connfd, buffer, sizeof(buffer));
+                puts(buffer);
+                int j = 0;
+                while (buffer[j]){
+                    buffer[j] = toupper(buffer[j]);
+                    j++;
+                }
+                write(connfd, (const char*)buffer, sizeof(buffer));
+                close(connfd);
+                exit(0);
+            }
+            close(connfd);
+        }
+        // if udp socket is readable receive the message.
+        if (FD_ISSET(udpfd, &rset)) {
+            len = sizeof(cliaddr);
+            bzero(buffer, sizeof(buffer));
+            printf("\nMessage from UDP client: ");
+            n = recvfrom(udpfd, buffer, sizeof(buffer), 0,(struct sockaddr*)&cliaddr, &len);
+            puts(buffer);
+            int j = 0;
+            while (buffer[j]){
+                buffer[j] = toupper(buffer[j]);
+                j++;
+            }
+            sendto(udpfd, (const char*)buffer, sizeof(buffer), 0,
+                   (struct sockaddr*)&cliaddr, sizeof(cliaddr));
+        }
+    }
+}
